b5982fda7eec19525e2dccadd951dcec3c064053
leasonlive <Administrator@PC201408310406>
2016-04-20 18:13:10 +0800

    增加一个添加流程的样例

--- src/main/webapp/js/jquery-rest.js
+++ src/main/webapp/js/jquery-rest.js
@@ -7,12 +7,7 @@ jQuery.extend({
 			type:'get',
 			data:''
 		});
-		//如果定义了异常处理方法，则不进行默认处理
-		$.extend(true, options, {
-			error:function (XMLHttpRequest, textStatus, errorThrown) {
-				$.restError(XMLHttpRequest, textStatus, errorThrown);
-			}
-		})
+		this.extendError(options);
 		this.ajax(options);
 	},
 	//执行rest的post方法，序列号参数对象
@@ -24,12 +19,7 @@ jQuery.extend({
 			type:'post',
 			data:JSON.stringify(options.data)
 		});
-		//如果定义了异常处理方法，则不进行默认处理
-		$.extend(true, options, {
-			error:function (XMLHttpRequest, textStatus, errorThrown) {
-				$.restError(XMLHttpRequest, textStatus, errorThrown);
-			}
-		})
+		this.extendError(options);
 		this.ajax(options);
 	},
 	//执行rest的put方法，序列号参数对象
@@ -40,12 +30,7 @@ jQuery.extend({
 			type:'put',
 			data:JSON.stringify(options.data)
 		});
-		//如果定义了异常处理方法，则不进行默认处理
-		$.extend(true, options, {
-			error:function (XMLHttpRequest, textStatus, errorThrown) {
-				$.restError(XMLHttpRequest, textStatus, errorThrown);
-			}
-		})
+		this.extendError(options);
 		this.ajax(options);
 	},
 	//执行rest的put方法，序列号参数对象
@@ -56,15 +41,20 @@ jQuery.extend({
 			type:'delete',
 			data:''
 		});
-		//如果定义了异常处理方法，则不进行默认处理
-		$.extend(true, options, {
-			error:function (XMLHttpRequest, textStatus, errorThrown) {
-				$.restError(XMLHttpRequest, textStatus, errorThrown);
-			}
-		})
+		this.extendError(options);
 		this.ajax(options);
 	},
+	//如果定义了异常处理方法，则不进行默认处理
+	extendError:function (options){
+		if (!options.error){
+			$.extend(true, options, {
+				error:function (XMLHttpRequest, textStatus, errorThrown) {
+					$.restError(XMLHttpRequest, textStatus, errorThrown);
+				}
+			})
+		}
+	},
 	restError : function (XMLHttpRequest, textStatus, errorThrown){
-		alert(JSON.parse(XMLHttpRequest.responseText).errmsg);
+		alert(JSON.parse(XMLHttpRequest.responseText).msg);
 	}
 });
